0d6489f38891c1edaa55ec3564b9851d58a1a61c
jony <nazrul229502@gmail.com>
2025-03-02 03:15:32 +0600

    added completed.js files

---
+++ completed.js
@@ -0,0 +1,139 @@
+
+document.getElementById('completed-btn-1').addEventListener('click',
+    function(){
+        
+        let count1=0;
+        let count2=0;
+        count1=count1+1;
+        const mileStone = document.getElementById("milestone").innerText;
+        const sum = count1+parseInt(mileStone);
+        document.getElementById("milestone").innerText=parseInt(sum);
+
+        count2++;
+        const taskAssign = document.getElementById("task-assign").innerText;
+        const minus = parseInt(taskAssign)-count2;
+        document.getElementById("task-assign").innerText=parseInt(minus);
+       
+        alert('Board Task Successfully');
+        if(minus==0){
+            alert('Congratulations!!!You have completed all the current task');
+        }
+        
+        document.getElementById('completed-btn-1').disabled= true;
+})
+
+document.getElementById('completed-btn-2').addEventListener('click',
+    function(){
+        
+        let count1=0;
+        let count2=0;
+        count1=count1+1;
+        const mileStone = document.getElementById("milestone").innerText;
+        const sum = count1+parseInt(mileStone);
+        document.getElementById("milestone").innerText=parseInt(sum);
+
+        count2++;
+        const taskAssign = document.getElementById("task-assign").innerText;
+        const minus = parseInt(taskAssign)-count2;
+        document.getElementById("task-assign").innerText=parseInt(minus);
+       
+        alert('Board Task Successfully');
+        if(minus==0){
+            alert('Congratulations!!!You have completed all the current task');
+        }
+       
+        document.getElementById('completed-btn-2').disabled= true;
+})
+
+document.getElementById('completed-btn-3').addEventListener('click',
+    function(){
+        
+        let count1=0;
+        let count2=0;
+        count1=count1+1;
+        const mileStone = document.getElementById("milestone").innerText;
+        const sum = count1+parseInt(mileStone);
+        document.getElementById("milestone").innerText=parseInt(sum);
+
+        count2++;
+        const taskAssign = document.getElementById("task-assign").innerText;
+        const minus = parseInt(taskAssign)-count2;
+        document.getElementById("task-assign").innerText=parseInt(minus);
+        
+        alert('Board Task Successfully');
+        if(minus==0){
+            alert('Congratulations!!!You have completed all the current task');
+        }
+    
+        document.getElementById('completed-btn-3').disabled= true;
+})
+
+document.getElementById('completed-btn-4').addEventListener('click',
+    function(){
+        
+        let count1=0;
+        let count2=0;
+        count1=count1+1;
+        const mileStone = document.getElementById("milestone").innerText;
+        const sum = count1+parseInt(mileStone);
+        document.getElementById("milestone").innerText=parseInt(sum);
+
+        count2++;
+        const taskAssign = document.getElementById("task-assign").innerText;
+        const minus = parseInt(taskAssign)-count2;
+        document.getElementById("task-assign").innerText=parseInt(minus);
+       
+        alert('Board Task Successfully');
+        if(minus==0){
+            alert('Congratulations!!!You have completed all the current task');
+        }
+        document.getElementById('completed-btn-4').disabled= true;
+})
+
+document.getElementById('completed-btn-5').addEventListener('click',
+    function(){
+        
+        let count1=0;
+        let count2=0;
+        count1=count1+1;
+        const mileStone = document.getElementById("milestone").innerText;
+        const sum = count1+parseInt(mileStone);
+        document.getElementById("milestone").innerText=parseInt(sum);
+
+        count2++;
+        const taskAssign = document.getElementById("task-assign").innerText;
+        const minus = parseInt(taskAssign)-count2;
+        document.getElementById("task-assign").innerText=parseInt(minus);
+       
+        alert('Board Task Successfully');
+        if(minus==0){
+            alert('Congratulations!!!You have completed all the current task');
+        }
+       
+        document.getElementById('completed-btn-5').disabled= true;
+})
+
+document.getElementById('completed-btn-6').addEventListener('click',
+    function(){
+        
+        let count1=0;
+        let count2=0;
+        count1=count1+1;
+        const mileStone = document.getElementById("milestone").innerText;
+        const sum = count1+parseInt(mileStone);
+        document.getElementById("milestone").innerText=parseInt(sum);
+
+        count2++;
+        const taskAssign = document.getElementById("task-assign").innerText;
+        const minus = parseInt(taskAssign)-count2;
+        document.getElementById("task-assign").innerText=parseInt(minus);
+       
+        alert('Board Task Successfully');
+        if(minus==0){
+            alert('Congratulations!!!You have completed all the current task');
+        }
+    
+        
+        document.getElementById('completed-btn-6').disabled= true;
+})
+
--- index.js
+++ index.js
@@ -2,3 +2,12 @@ document.getElementById("discover-something").addEventListener('click',
     function(){
        window.location.href="./blog.html";
 })
+
+document.getElementById("random-color").addEventListener('click',
+    function(){
+       const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+       document.body.style.backgroundColor = randomColor;
+})
+
+
+      document.completedBtnID("completed-btn-1");
